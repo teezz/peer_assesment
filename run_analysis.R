1245dca596f2859525c48fe07383a2d045582634
teezz <info@reporting-online.com>
2015-10-25 13:46:30 -0400

    Load required packages automatically

--- run_analysis.R
+++ run_analysis.R
@@ -4,8 +4,12 @@
 
 
 run_analysis <- function() {
+        list.of.packages <- c("dplyr", "data.table")
+        new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+        if(length(new.packages)) install.packages(new.packages)
         library(dplyr)
         library(data.table)
+
         source("createDataset.R")
         source("assignColumnNames.R")
         source("setActivityNames.R")
